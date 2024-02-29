eb0da6feb415d05db9d66656be68ff1bde750f27
EvianMckeown <evian.mck@gmail.com>
2024-02-29 20:34:55 +0200

    Preferences functionality added to server code

--- client.py
+++ client.py
@@ -1,63 +1,70 @@
 import socket
 
 HEADER = 512
-FORMAT = 'utf-8'
-USERNAME = 2048
+FORMAT = "utf-8"
+USERNAME = 256
+PREFS = 256
 
-#MSG that if found closes the connection to the client
+# MSG that if found closes the connection to the client
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
 HOST = socket.gethostbyname(socket.gethostname())
-PORT = 6463 #Specific port for Server-Client communication
+PORT = 6463  # Specific port for Server-Client communication
 ADDR = (HOST, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
 
 def send(msg):
     """_summary_
 
     Args:
         msg (_type_): _description_
-    """    
-    #HEADER
+    """
+    # HEADER
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     # Now pad to HEADER length
-    send_length += b' ' * (HEADER - len(send_length))
+    send_length += b" " * (HEADER - len(send_length))
     client.send(send_length)
-    
-    #USERNAME
+
+    # USERNAME
     username = "BrafMeToo"
     username = "BrafMeToo".encode(FORMAT)
     username_length = len(username)
     # Now pad to USERNAME length
-    username += b' ' * (USERNAME - (username_length))
+    username += b" " * (USERNAME - (username_length))
     client.send(username)
-    
-    #TODO: Change client settings
+
+    # TODO: Change client settings
     # Encryption=Y/N | Discoverable=Y/N |
-    # Encryption: Asymmetric public-private key used to encrypt shared private RSA key, 
-            # which is then used to encrypt to data between clients. 
-    
-    
+    # Encryption: Asymmetric public-private key used to encrypt shared private RSA key,
+    # which is then used to encrypt to data between clients.
+    encryption = False
+    discoverable = True
+    prefs = (str(int(encryption)) + str(int(discoverable))).encode(FORMAT)
+    prefs += b" " * (PREFS - len(prefs))
+    client.send(prefs)
+
     client.send(message)
     print("MESSAGE= '" + str(message) + "'")
-        
+
+
 def recv_msg():
-    """_summary_
-    """    
+    """_summary_"""
     msg_length = client.recv(HEADER).decode(FORMAT)
-    if msg_length: #If !msg_lenth.equals(null)
+    if msg_length:  # If !msg_lenth.equals(null)
         msg_length = int(msg_length)
-        
+
         #
-        
+
         msg = client.recv(msg_length).decode(FORMAT)
-        #print("MSG NON EMPTY") #DEBUG
+        # print("MSG NON EMPTY") #DEBUG
         print(msg)
-    
+
+
 send("Hello World!")
 while 1:
     recv_msg()
@@ -65,4 +72,4 @@ while 1:
 # Need loop to be able to send and receive indefinitely
 # Then it breaks out when a disconnect/close is requested
 
-#send(DISCONNECT_MESSAGE)
+# send(DISCONNECT_MESSAGE)
--- server.py
+++ server.py
@@ -3,17 +3,19 @@ import threading
 
 # get local ip address and set port to use
 HOST = socket.gethostbyname(socket.gethostname())
-PORT = 6463 # use specific port for TCP
+PORT = 6463  # use specific port for TCP
 ADDR = (HOST, PORT)
 
 HEADER = 512
-USERNAME = 2048
-FORMAT = 'utf-8'
+USERNAME = 256
+PREFS = 256
+FORMAT = "utf-8"
 
-#MSG that if found closes the connection to the client
+# MSG that if found closes the connection to the client
 DISCONNECT_MESSAGE = "!DISCONNECT"
-LISTENER_LIMIT = 15 #TODO: Set Limit if required
-active_clients = [] # All currently connected users
+LISTENER_LIMIT = 15  # TODO: Set Limit if required
+active_clients = []  # All currently connected users
+
 
 def send_list_of_connections(conn, addr):
     """Send list of connections (that are discoverable) to a specific client
@@ -22,13 +24,14 @@ def send_list_of_connections(conn, addr):
         conn (socket): Socket object of connection to client
         addr (_RetAddress): Return address part of socket object
     """
-    
+
     user_list = ""
     for user in active_clients:
         user_list += "|".join([f"{user[0]}:{user[1]}:{user[2]}"])
-    #user_list = "|".join([f"{user[0]}:{user[1]}" for user in active_clients])
-    send_msgtoclient(user_list, conn, addr) 
-    #print(user_list)
+    # user_list = "|".join([f"{user[0]}:{user[1]}" for user in active_clients])
+    send_msgtoclient(user_list, conn, addr)
+    # print(user_list)
+
 
 def handle_client(conn, addr):
     """Initiate and handle connection to a specific client
@@ -37,39 +40,44 @@ def handle_client(conn, addr):
         conn (socket): Socket object of connection to client
         addr (_RetAddress): Return address part of socket object
     """
-    
+
     # parallel client connection
     print(f"[New Connection] {addr} connected.")
     connected = True
-    
+
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length: #If !msg_lenth.equals(null)
+        if msg_length:  # If !msg_lenth.equals(null)
             msg_length = int(msg_length)
-            
-            #TODO: Read username for client
+
+            # TODO: Read username for client
             username = conn.recv(USERNAME).decode(FORMAT).strip()
+
+            # Encryption=Y/N | Discoverable=Y/N |
+            # Encryption: Asymmetric public-private key used to encrypt shared private RSA key,
+            # which is then used to encrypt to data between clients.
+            prefs = conn.recv(PREFS).decode(FORMAT).strip()
+            # print("PREFS= '" + prefs + "'")
+
             if username:
-                active_clients.append((username, addr, prefs)) 
-            
-            #TODO?
-            #Change client settings
-            
-            #TODO?
-            #
-            
+                active_clients.append((username, addr, prefs))
+
+            # TODO?
+            # Change client settings
+
             msg = conn.recv(msg_length).decode(FORMAT)
             if msg == DISCONNECT_MESSAGE:
                 connected = False
                 print(f"[Closing Connection] {addr}")
             else:
                 print(f"[{addr}] {msg}")
-                
-                #example server to client msg
+
+                # example server to client msg
                 send_msgtoclient("THIS IS A TEST MESSAGE", conn, addr)
                 send_list_of_connections(conn, addr)
-            
+
     conn.close()
+
 
 def send_msgtoclient(msg, conn, addr):
     """Send a message to a client through TCP, with the correct communication protocol
@@ -79,33 +87,35 @@ def send_msgtoclient(msg, conn, addr):
         conn (socket): Socket object of connection to client
         addr (_RetAddress): Return address part of socket object
     """
-    
+
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     # pad to header length
-    send_length += b' ' * (HEADER - len(send_length))
+    send_length += b" " * (HEADER - len(send_length))
     conn.send(send_length)
     conn.send(message)
 
+
 def main():
-    
+
     # TCP uses SOCK_STREAM
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # UDP uses SOCK_DGRAM
     # server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server.bind(ADDR)
-    
+
     print("Starting server...")
     server.listen(LISTENER_LIMIT)
     print(f"Listening on {HOST}:{PORT}")
-    
+
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr)).start()
-        
+
         print(f"[No. of Active Connections] {threading.active_count() - 1}")
-    
-#call main
-if __name__ == '__main__':
+
+
+# call main
+if __name__ == "__main__":
     main()
